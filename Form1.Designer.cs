2f9ed75614edf5729be39b1756b325824edf4dd0
Syed Talha Hai <talhahai@yahoo.com>
2017-01-21 19:15:12 +0500

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace FileRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.button2.Location = new System.Drawing.Point(123, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Rename";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.textBox3.Location = new System.Drawing.Point(134, 136);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(131, 29);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.textBox4.Location = new System.Drawing.Point(134, 171);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(131, 29);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.radioButton1.Location = new System.Drawing.Point(12, 172);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(120, 28);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.Text = "Delete Last";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.radioButton2.Location = new System.Drawing.Point(12, 137);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(127, 28);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.Text = "Delete First ";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.radioButton3.Location = new System.Drawing.Point(12, 68);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(98, 28);
+            this.radioButton3.TabIndex = 14;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Replace";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.textBox5.Location = new System.Drawing.Point(116, 67);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(149, 29);
+            this.textBox5.TabIndex = 15;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.label5.Location = new System.Drawing.Point(77, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 24);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "To";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.textBox6.Location = new System.Drawing.Point(116, 102);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(149, 29);
+            this.textBox6.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.label1.Location = new System.Drawing.Point(6, 208);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 36);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Status: None";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 253);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.radioButton3);
+            this.Name = "Form1";
+            this.Text = "File Renamer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FileRenamer
+{
+    public partial class Form1 : Form
+    {
+        string FolderPath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Status: In Progress";
+            button1.Enabled = false;
+            string[] Files = Directory.GetFiles(FolderPath);
+            if (radioButton3.Checked)
+            {
+                foreach (string OldFile in Files)
+                {
+                    string NewName = OldFile.Replace(textBox5.Text, textBox6.Text);
+                    File.Move(OldFile, NewName);
+                }
+            }
+            if (radioButton2.Checked) //Delete First Characters
+            {
+                foreach (string OldFile in Files)
+                {
+                    string NewFile = OldFile.Substring(Convert.ToInt32(textBox3.Text), OldFile.Length);
+                    File.Move(OldFile, NewFile);
+                }
+            }
+            if (radioButton1.Checked) //Delete Last Characters
+            {
+                foreach (string OldFile in Files)
+                {
+                    string Ext = OldFile.Substring(OldFile.Length - 4, 4);
+                    string NewFile = OldFile.Substring(0, OldFile.Length - Convert.ToInt32(textBox4.Text) - 4) + Ext;
+                    File.Move(OldFile, NewFile);
+                }
+            }
+            button1.Enabled = true;
+            label1.Text = "Status: Successful";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                button2.Enabled = true;
+                FolderPath = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox3.Enabled = radioButton2.Checked;
+            textBox5.Enabled = textBox6.Enabled = textBox4.Enabled = !radioButton2.Checked;
+            textBox5.BackColor = textBox6.BackColor = textBox4.BackColor = Color.Empty;
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox3.Text, "^[0-9]+$"))
+            {
+                button2.Enabled = true;
+                textBox3.BackColor = Color.Empty;
+            }
+            else
+            {
+                button2.Enabled = false;
+                textBox3.BackColor = Color.Tomato;
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox5.Enabled = textBox6.Enabled = radioButton3.Checked;
+            textBox3.Enabled = textBox4.Enabled = !radioButton3.Checked;
+            textBox3.BackColor = textBox4.BackColor = Color.Empty;
+            if (textBox5.Text.Length > 0)
+            {
+                button2.Enabled = true;
+                textBox5.BackColor = Color.Empty;
+            }
+            else
+            {
+                button2.Enabled = false;
+                textBox5.BackColor = Color.Tomato;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox4.Enabled = radioButton1.Checked;
+            textBox5.Enabled = textBox6.Enabled = textBox3.Enabled = !radioButton1.Checked;
+            textBox5.BackColor = textBox6.BackColor = textBox3.BackColor = Color.Empty;
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox4.Text, "^[0-9]+$"))
+            {
+                button2.Enabled = true;
+                textBox4.BackColor = Color.Empty;
+            }
+            else
+            {
+                button2.Enabled = false;
+                textBox4.BackColor = Color.Tomato;
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox4.Text, "^[0-9]+$"))
+            {
+                button2.Enabled = true;
+                textBox4.BackColor = Color.Empty;
+            }
+            else
+            {
+                button2.Enabled = false;
+                textBox4.BackColor = Color.Tomato;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox3.Text, "^[0-9]+$"))
+            {
+                button2.Enabled = true;
+                textBox3.BackColor = Color.Empty;
+            }
+            else
+            {
+                button2.Enabled = false;
+                textBox3.BackColor = Color.Tomato;
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox5.Text.Length > 0)
+            {
+                button2.Enabled = true;
+                textBox5.BackColor = Color.Empty;
+            }
+            else
+            {
+                button2.Enabled = false;
+                textBox5.BackColor = Color.Tomato;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (textBox5.Text.Length > 0)
+            {
+                button2.Enabled = true;
+                textBox5.BackColor = Color.Empty;
+            }
+            else
+            {
+                button2.Enabled = false;
+                textBox5.BackColor = Color.Tomato;
+            }
+        }
+    }
+}
